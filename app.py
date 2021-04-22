1fe7737e34d0b54b68977c5c9e0130281294781a
Бабин Даниил <daniilbi99@gmail.com>
2021-04-22 20:59:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,35 @@
 from flask import Flask
+import yaml
 import sys
 app = Flask(__name__)
 
 var = []
 names = []
 
-# def main(argv):
-# #     names = argv[1]
-# #     print(names)
-#     app.run()
-
 @app.route('/')
 def hello_world():
-    return 'Hello, fucktard!\nHere r sum namez:'
-#     return 'Hello, fucktard!\nHere r sum namez: {0}\n'.format(names)
+    return 'Hello, fucktard!\n{0}'.format(names)
 
-# @app.route('/{0}'.format(names[0]))
-# def hello_world():
-#     return 'Hello, {}!\n'.format(names[0])
+@app.route('/{0}'.format(names[0]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[0])
 
-# @app.route('/{0}'.format(names[1]))
-# def hello_world():
-#     return 'Hello, {}!\n'.format(names[1])
+@app.route('/{0}'.format(names[1]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[1])
 
-# @app.route('/{0}'.format(names[2]))
-# def hello_world():
-#     return 'Hello, {}!\n'.format(names[2])
+@app.route('/{0}'.format(names[2]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[2])
 
-# @app.route('/{0}'.format(names[3]))
-# def hello_world():
-#     return 'Hello, {}!\n'.format(names[3])
-
+@app.route('/{0}'.format(names[3]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[3])
 
 if __name__ == '__main__':
-#     main(sys.argv)
+    with open('test.yml') as f:
+        var = yaml.load(f)
+        var = {k: v[0] for d in var for k, v in d.items()}
+    names = var['roles']['hello_pages']
+    # print(names)
     app.run()
