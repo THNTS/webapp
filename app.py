5f80e9a5d1324b90e5b0abc483ee8538fcf50ba4
Бабин Даниил <daniilbi99@gmail.com>
2021-04-22 20:12:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,30 +7,27 @@ names = []
 
 @app.route('/')
 def hello_world():
-    return 'Hello, fucktard!\n'
+    return 'Hello, fucktard!\nHere r sum namez: {0}\n'.format(names)
 
-@app.route('/{0}'.format(names[0]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[0])
+# @app.route('/{0}'.format(names[0]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[0])
 
-@app.route('/{0}'.format(names[1]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[1])
+# @app.route('/{0}'.format(names[1]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[1])
 
-@app.route('/{0}'.format(names[2]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[2])
+# @app.route('/{0}'.format(names[2]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[2])
 
-@app.route('/{0}'.format(names[3]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[3])
+# @app.route('/{0}'.format(names[3]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[3])
 
 def main(argv):
-    # with open('test.yml') as f:
-    #     var = yaml.load(f)
-    #     var = {k: v[0] for d in var for k, v in d.items()}
-    # names = var['roles']['hello_pages']
     names = argv[1]
+    print(names)
     app.run()
 
 if __name__ == '__main__':
