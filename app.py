8481ad55384fca65dd667e0e52d355b248e758d5
Бабин Даниил <daniilbi99@gmail.com>
2021-04-22 22:01:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,35 @@ app = Flask(__name__)
 # print(names)
 names = ['world', 'test', 'toast', 'mda', 'nothing']
 
-
-@app.route('/')
-def hello_world():
+try:
+    with open('/opt/example_app/names') as f:
+        var = f.read()
+        names = eval(var)
+    print(names)
+    @app.route('/')
     return 'Hello, fucktard!\n'
+except:
+    return "FUCK U"
+    
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, fucktard!\n'
 
-@app.route('/{0}'.format(names[0]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[0])
+# @app.route('/{0}'.format(names[0]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[0])
 
-@app.route('/{0}'.format(names[1]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[1])
+# @app.route('/{0}'.format(names[1]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[1])
 
-@app.route('/{0}'.format(names[2]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[2])
+# @app.route('/{0}'.format(names[2]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[2])
 
-@app.route('/{0}'.format(names[3]))
-def hello_world():
-    return 'Hello, {}!\n'.format(names[3])
+# @app.route('/{0}'.format(names[3]))
+# def hello_world():
+#     return 'Hello, {}!\n'.format(names[3])
 
 if __name__ == '__main__':
     app.run()
