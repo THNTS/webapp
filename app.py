1ed563465d3ad53a759b88171cb75dbf220d0ec3
Бабин Даниил <daniilbi99@gmail.com>
2021-04-22 06:45:07 +0300

    adding files

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+import yaml
+app = Flask(__name__)
+
+var = []
+names = []
+
+@app.route('/')
+def hello_world():
+    return 'Hello, fucktard!\n'
+
+@app.route('/'.format(names[0]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[0])
+
+@app.route('/'.format(names[1]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[1])
+
+@app.route('/'.format(names[1]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[2])
+
+@app.route('/'.format(names[1]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[3])
+
+if __name__ == '__main__':
+    with open('test.yml') as f:
+        var = yaml.load(f)
+        var = {k: v[0] for d in var for k, v in d.items()}
+    names = var['roles']['hello_pages']
+    app.run()
+
+#
