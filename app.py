5ea968c2e07e9a230805bbbbc36dfc6662090341
Бабин Даниил <daniilbi99@gmail.com>
2021-04-22 23:41:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ with open('/opt/example_app/names') as f:
     var = f.read()
     names = eval(var)
      
-@app.route('/')
-def hello_world():
-    try:
-#         with open('/opt/example_app/names') as f:
-#             var = f.read()
-#             names = eval(var)
-        return f'Hello, fucktard!\n{names}\n'
-    except:
-        return "FUCK U"
+# @app.route('/')
+# def hello_world():
+#     try:
+# #         with open('/opt/example_app/names') as f:
+# #             var = f.read()
+# #             names = eval(var)
+#         return f'Hello, fucktard!\n{names}\n'
+#     except:
+#         return "FUCK U"
     
 # @app.route('/')
 # def hello_world():
@@ -40,6 +40,10 @@ def hello_world_3():
 @app.route('/{0}'.format(names[4]))
 def hello_world_4():
     return 'Hello, {}!\n'.format(names[4])
+
+# @app.errorhandler(404)
+# def page_not_found(error):
+#    return render_template('404.html', title = '404'), 404
 
 if __name__ == '__main__':
     app.run()
