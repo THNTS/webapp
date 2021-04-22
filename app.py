306082ea5660296d1e913be375b778c43cec9a9a
Бабин Даниил <daniilbi99@gmail.com>
2021-04-22 22:19:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ app = Flask(__name__)
 
 # names = ['world', 'test', 'toast', 'mda', 'nothing']
 
-# with open('/opt/example_app/names') as f:
-#     var = f.read()
-#     names = eval(var)
-# print(names)
+names = []
+
 @app.route('/')
 def hello_world():
     try:
-        with open('/opt/example_app/names') as f:
-            var = f.read()
-            names = eval(var)
+#         with open('/opt/example_app/names') as f:
+#             var = f.read()
+#             names = eval(var)
         return f'Hello, fucktard!\n{names}\n'
     except:
         return "FUCK U"
@@ -21,9 +19,9 @@ def hello_world():
 # def hello_world():
 #     return 'Hello, fucktard!\n'
 
-# @app.route('/{0}'.format(names[0]))
-# def hello_world():
-#     return 'Hello, {}!\n'.format(names[0])
+@app.route('/{0}'.format(names[0]))
+def hello_world():
+    return 'Hello, {}!\n'.format(names[0])
 
 # @app.route('/{0}'.format(names[1]))
 # def hello_world():
@@ -38,4 +36,7 @@ def hello_world():
 #     return 'Hello, {}!\n'.format(names[3])
 
 if __name__ == '__main__':
+    with open('/opt/example_app/names') as f:
+        var = f.read()
+        names = eval(var)
     app.run()
